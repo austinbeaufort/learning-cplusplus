29ec48ae19aeb55e7b534e420dae32c563e3c8aa
austinbeaufort <austinbeaufort7@gmail.com>
2020-06-25 11:21:41 -0400

    dynamic memory allocation

--- main.cpp
+++ main.cpp
@@ -5,28 +5,145 @@
 using namespace std;
 
 
+// DYNAMIC MEMORY ALLOCATION
+
+// EXAMPLE 4
+// int main()
+// {
+//     size_t size {0};
+//     double *temp_ptr {nullptr};
+
+//     cout << "How many temperatures? ";
+//     cin >> size;
+
+
+//     temp_ptr = new double[size];
+//     // mem address of the first double in temp_ptr
+//     cout << temp_ptr << endl;
+
+//     delete [] temp_ptr;
+// }
+
+
+
+// EXAMPLE 3
+// int main()
+// {
+//     int *int_ptr {nullptr};
+//     // prints 0
+//     cout << int_ptr;
+//     // allocate new mem address for int_ptr on heap
+//     int_ptr = new int;
+//     // print mem location of int on the heap
+//     cout << int_ptr;
+//     // frees memory
+//     delete int_ptr;
+// }
+
+
+
+// EXAMPLE 2
+// int main()
+// {
+//     int *array_ptr {nullptr};
+//     int size {};
+
+//     cout << "How big do you want the array? ";
+//     cin >> size;
+
+//     // allocating array memory on the heap
+//     array_ptr = new int[size];
+
+//     cout << array_ptr;
+
+//     // free the memory
+//     delete [] array_ptr;
+// }
+
+
+
+// EXAMPLE 1
+// int main()
+// {
+//     int *int_ptr {nullptr};
+//     // uses memory from heap
+//     int_ptr = new int;
+
+//     // get address of uninitialized int
+//     cout << int_ptr << endl;
+
+//     *int_ptr = 100;
+
+//     cout << *int_ptr << endl;
+
+//     // deallocate storage
+//     delete int_ptr;
+// }
+
+
+
+
+
+
+
+
 
 // DEREFERENCING A POINTER
 
+// EXAMPLE 4
+// int main()
+// {
+//     vector<string> stooges {"Larry", "Moe", "Curly"};
+//     vector<string> *vector_ptr {nullptr};
+
+//     vector_ptr = &stooges;
+//     cout << "First stooge: " << (*vector_ptr).at(0) << endl;
+
+//     cout << "Stooges:";
+//     for (auto stooge: *vector_ptr)
+//         cout << stooge << " ";
+// }
+
+
+// EXAMPLE 3
+// int main()
+// {
+//     string name {"Frank"};
+//     string *string_ptr {&name};
+
+//     // mem address
+//     cout << string_ptr << endl;
+//     // dereferencing, getting actual value
+//     cout << *string_ptr << endl;
+
+//     name = "James";
+//     // same address
+//     cout << string_ptr << endl;
+//     // when dereferenced, name is now James
+//     cout << *string_ptr << endl;
+// }
+
+
+
 // EXAMPLE 2
-int main()
-{
-    double high_temp {100.7};
-    double low_temp {37.4};
-    double *temp_ptr {&high_temp};
+// int main()
+// {
+//     double high_temp {100.7};
+//     double low_temp {37.4};
+//     double *temp_ptr {&high_temp};
 
-    // memory address
-    cout << temp_ptr << endl;
-    // dereferencing, getting actual value
-    cout << *temp_ptr << endl;
+//     // memory address
+//     cout << temp_ptr << endl;
+//     // dereferencing, getting actual value
+//     cout << *temp_ptr << endl;
 
-    // now points to low_temp memory address
-    temp_ptr = &low_temp;
-    // low temp memory address
-    cout << temp_ptr << endl;
-    // dereferencing to get low temp value
-    cout << *temp_ptr << endl;
-}
+//     // now points to low_temp memory address
+//     temp_ptr = &low_temp;
+//     // low temp memory address
+//     cout << temp_ptr << endl;
+//     // dereferencing to get low temp value
+//     cout << *temp_ptr << endl;
+// }
 
 
 
