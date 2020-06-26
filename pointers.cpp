a586fbbcef5be7e58f69f03736fca3965f3ab31c
austinbeaufort <austinbeaufort7@gmail.com>
2020-06-26 16:30:01 -0400

    returning a pointer from a function

--- pointers.cpp
+++ pointers.cpp
@@ -7,6 +7,60 @@ using namespace std;
 
 // RETURNING A POINTER FROM A FUNCTION
 
+// EXAMPLE 3
+int *create_array(size_t size, int init_value = 0)
+{
+    int *new_storage = new int[size];
+    for (size_t i{0}; i < size; i++)
+        *(new_storage + i) = init_value;
+    return new_storage;
+}
+
+
+
+
+
+
+
+
+// EXAMPLE 2
+// int *create_array(size_t size, int init_value = 0)
+// {
+//     int *new_storage {nullptr};
+//     new_storage = new int[size];
+
+//     for (size_t i {0}; i < size; i++)
+//         *(new_storage + i) = init_value;
+//     return new_storage;
+// }
+
+// int main()
+// {
+//     int *my_array = create_array(100, 20);
+
+//     delete [] my_array;
+// }
+
+
+// EXAMPLE 1
+// int *largest_int(int *int_ptr1, int *int_ptr2)
+// {
+//     if (*int_ptr1 > *int_ptr2)
+//         return int_ptr1;
+//     else
+//         return int_ptr2;
+// }
+
+
+// int main()
+// {
+//     int a {100};
+//     int b {200};
+
+//     int *largest_ptr = largest_int(&a, &b);
+//     cout << *largest_ptr << endl;
+// }
+
 
 
 
